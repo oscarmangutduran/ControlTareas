47fef46aba08293dffbcf52fade7ae91df2d06e0
Oscar Mangut <mangutduranoscar@gmail.com>
2024-03-19 18:23:14 +0100

    Proyecto finalizado

--- api.php
+++ api.php
@@ -1,40 +1,55 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$password = "";
-$bbdd = "app";
+header("Content-Type: application/json");
+$servidor="localhost";
+$usuario="root";
+$password="";
+$basededatos="app";
 
-$conexion = new mysqli($servidor, $usuario, $password, $bbdd);
+$conexion=new mysqli($servidor,$usuario,$password,$basededatos);
 
-if (!$conexion->connect_error) {
-    echo "Conexión establecida<br>";
-} else {
+if($conexion->connect_error){
     print_r($conexion->connect_error);
 }
 
-if ($_POST) {
-    $tarea = $_POST["tarea"];
+$accion=$_POST["accion"];
 
-    // Consulta SQL con consulta preparada
-    $sql = "INSERT INTO `tareas` (`id`, `tarea`, `completada`) VALUES (NULL, ?, '0')";
+//Create
+if($accion=="agregar"){
+    $tarea=$_POST["tarea"];
+    $sql="INSERT INTO tareas (id,tarea,completado) 
+    VALUES(NULL, '$tarea', '0');";
+    $conexion->query($sql);
+    echo json_encode(array('success'=>true,'datos'=> "Se agregó la tarea."));
+}
+//Delete
+if($accion=="borrar"){
+    $id=$_POST["id"];
+    $sql="DELETE FROM tareas WHERE id= $id";
+    $conexion->query($sql);
+    echo json_encode(array('success'=>true,'datos'=> "Se eliminó la tarea."));
+}
+//Update
+if($accion=="completar"){
+    $id=$_POST["id"];
 
-    // Preparar la consulta
-    $stmt = $conexion->prepare($sql);
+    $sql="UPDATE tareas SET completado=1 WHERE id= $id";
+    $conexion->query($sql);
+    echo json_encode(array('success'=>true,'datos'=> "Se completó la tarea."));
+}
+//Read
 
-    // Vincular parámetros
-    $stmt->bind_param("s", $tarea);
+if($accion=="leer"){
+    $sql="SELECT * FROM tareas";
+    $resultado=$conexion->query($sql);
+        if($resultado->num_rows>0){
+            $tareas=array();
+            while($fila=$resultado->fetch_assoc()){
+                $tareas[]=$fila;
+            }
+    echo json_encode(array('success'=>true,'datos'=> $tareas));
 
-    // Ejecutar la consulta
-    if ($stmt->execute()) {
-        echo "Se agregó la tarea: " . $tarea;
-    } else {
-        echo "Error al agregar la tarea: " . $conexion->error;
-    }
-
-    // Cerrar la consulta preparada
-    $stmt->close();
+}
 }
 
-// Cerrar la conexión
-$conexion->close();
+
 ?>
