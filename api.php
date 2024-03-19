4e95d2d85200b6ad75b08e8313c2e78b22ab3d3e
Oscar Mangut <mangutduranoscar@gmail.com>
2024-03-19 14:25:58 +0100

    Conectándose a una base de datos Mysql y php

--- api.php
+++ api.php
@@ -1,6 +1,22 @@
 <?php
+$servidor = "localhost";
+$usuario = "root"; // Corregido de $usuarios a $usuario
+$password = "";
+$bbdd = "app";
 
-    if($_POST) {
-        echo "Se agregó la tarea: ".$_POST["tarea"];
-    }
+$conexion = new mysqli($servidor, $usuario, $password, $bbdd);
+
+// Verificar errores de conexión
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+} else {
+    echo "Conexión establecida<br>";
+}
+
+if ($_POST && isset($_POST["tarea"])) { // Verificar si se ha enviado algo mediante POST
+    $tarea = $_POST["tarea"];
+    echo "Se agregó la tarea: " . $tarea;
+} else {
+    print_r($conexion->connect_error);
+}
 ?>
